785f55194eb76c22d61eba34b981325c924f897a
kter <me@kter.jp>
2017-03-02 23:14:52 +0900

    nameカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302135240) do
+ActiveRecord::Schema.define(version: 20170302141004) do
 
   create_table "employees", force: :cascade do |t|
-    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "email"
