94873feb71c659727a7a7713ebd7a5840d546c66
kter <me@kter.jp>
2017-03-02 23:08:25 +0900

    名前の形式を変更

--- db/migrate/20170302134337_add_email_to_employee.rb
+++ db/migrate/20170302134337_add_email_to_employee.rb
@@ -1,5 +1,9 @@
 class AddEmailToEmployee < ActiveRecord::Migration[5.0]
-  def change
+  def up
     add_column :employees, :email, :string
+  end
+
+  def down
+    remove_column :employees, :email, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302134337) do
+ActiveRecord::Schema.define(version: 20170302135240) do
 
   create_table "employees", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "email"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
 end
