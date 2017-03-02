0cefb4ff0fe884b973050602ac97a476d7445f14
kter <me@kter.jp>
2017-03-02 23:32:47 +0900

    バリデーションを追加

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,7 @@
 class Employee < ApplicationRecord
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :email, presence: true
 
   def full_name
     first_name + " " + last_name
