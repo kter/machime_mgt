57bc6361583acc58c853851aef67b5031dfecaa4
kter <me@kter.jp>
2017-03-02 23:41:48 +0900

    テスト追加

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class EmployeeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @employee = Employee.new(first_name: 'firstname', last_name: 'lastname', email: 'user@example.com')
+  end
+
+  test "should be valid" do
+    assert @employee.valid?
+  end
+
+  test "first_name should be present" do
+    @employee.first_name = ''
+    assert_not @employee.valid?
+  end
+
+  test "last_name should be present" do
+    @employee.last_name = '   '
+    assert_not @employee.valid?
+  end
+
+  test "email should be present" do
+    @employee.email = ''
+    assert_not @employee.valid?
+  end
 end
