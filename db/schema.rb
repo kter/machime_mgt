dfd93ff7afac1d10882fb9233f881389a2d411f2
kter <me@kter.jp>
2017-03-04 12:49:42 +0900

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304033930) do
+ActiveRecord::Schema.define(version: 20170304034805) do
 
   create_table "employees", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20170304033930) do
     t.string   "email"
     t.string   "first_name"
     t.string   "last_name"
+  end
+
+  create_table "preferences", force: :cascade do |t|
+    t.string   "domain"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "softwares", force: :cascade do |t|
